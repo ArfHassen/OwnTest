6ab331e0fb0abab13365eb7ca55a11d8a2655390
hassen <hassen.arfaoui@logistafrance.fr>
2020-10-06 15:26:03 +0200

    init first commit

---
+++ src/pack2020/ThreadOnCollection.java
@@ -0,0 +1,22 @@
+package pack2020;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class ThreadOnCollection {
+    public static void main(String[] args) {
+        synchronizedCollectionExemple();
+    }
+
+    private static void synchronizedCollectionExemple() {
+        Collection<Integer> integers = Collections.synchronizedCollection(new ArrayList<>());
+        Thread thread_1 = new Thread(()->integers.addAll(Arrays.asList(1,2,3,4,5)));
+        Thread thread_2 = new Thread(()->integers.addAll(Arrays.asList(6,7,8,9,10)));
+        thread_1.start();
+        thread_2.start();
+        System.out.println(integers);
+    }
+
+
+}
